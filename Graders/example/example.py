47bcb196a7343c4133810bdf122ecc81f8742608
David Shen <me@pantherman594.com>
2019-01-22 20:33:52 -0500

    Have example grader actually do something (open atom for repo)

--- Graders/example/example.py
+++ Graders/example/example.py
@@ -4,7 +4,8 @@ import os
 
 class Grader:
     def __init__(self):
-        self.process = subprocess.Popen("echo Example grader running from {}".format(os.getcwd()), shell=True)
+        # self.process = subprocess.Popen("echo Example grader running from {}".format(os.getcwd()), shell=True)
+        self.process = subprocess.Popen("atom {}".format(os.getcwd()), shell=True)
 
     def cleanup(self):
         self.process.kill()
